4e882408a86fa101ab7b038ba1dc4ad325ac0f23
Sheikh Ahmadullah <115889131+SK-AHMD@users.noreply.github.com>
2022-11-28 20:09:37 +0530

    Create 1665A - GCD vs LCM.cpp

---
+++ Solutions/1665A - GCD vs LCM.cpp
@@ -0,0 +1,15 @@
+//sk 
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+          int t;
+          cin>>t;
+          while(t--){
+                    int n;
+                    cin>>n;
+                    cout<<1<<" "<<n-3<<" "<<1<<" "<<1<<"\n";
+                    
+          }
+          return 0;
+          
+}
