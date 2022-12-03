1b8ebf50d2c04afb472d4c0a20d27d5dabac8a98
Sheikh Ahmadullah <115889131+SK-AHMD@users.noreply.github.com>
2022-12-03 23:51:55 +0530

    Create Q6 [1555A] PizzaForces.cpp

---
+++ C2Ladder/@900/Q6 [1555A] PizzaForces.cpp
@@ -0,0 +1,11 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+    long long n,t;
+    int main()
+    {
+    	for(cin>>t;t--;)
+    	{
+    		cin>>n;
+    		cout<<max(15LL,(n+1)/2*5)<<endl;
+    	}
+    }
