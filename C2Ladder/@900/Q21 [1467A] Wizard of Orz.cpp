11666c4dafc49684890215d2a5a98cf09f588945
Sheikh Ahmadullah <115889131+SK-AHMD@users.noreply.github.com>
2022-12-03 23:59:13 +0530

    Create Q21 [1467A] Wizard of Orz.cpp

---
+++ C2Ladder/@900/Q21 [1467A] Wizard of Orz.cpp
@@ -0,0 +1,21 @@
+ 
+// sk
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        cout<<9;
+
+        for(int i=0;i<n-1;i++){
+            cout<<(8+i)%10;
+
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
