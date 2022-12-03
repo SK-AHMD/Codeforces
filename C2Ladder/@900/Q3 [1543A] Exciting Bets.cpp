6559accbe9711d6e0a3143e12ccb5e940c878844
Sheikh Ahmadullah <115889131+SK-AHMD@users.noreply.github.com>
2022-12-03 23:45:28 +0530

    Create Q3 [1543A] Exciting Bets.cpp

---
+++ C2Ladder/@900/Q3 [1543A] Exciting Bets.cpp
@@ -0,0 +1,16 @@
+// sk
+#include <bits/stdc++.h>
+using namespace std;
+int main( ){
+    int t;
+    cin>>t;
+    while(t--){
+        long long a,b;
+        cin>>a>>b;
+        long long c = abs(a-b);
+        if(c==0) cout<<"0 0\n";
+        else cout<<c<<" "<<min(a%c,c-(a%c))<<"\n";
+        
+    }
+  return 0;
+}
