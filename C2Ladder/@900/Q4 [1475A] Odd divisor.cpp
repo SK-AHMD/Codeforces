d008b1e9d3787d5aa4d9c170ffe981269cb974d5
Sheikh Ahmadullah <115889131+SK-AHMD@users.noreply.github.com>
2022-12-03 23:46:54 +0530

    Create Q4 [1475A] Odd divisor.cpp

---
+++ C2Ladder/@900/Q4 [1475A] Odd divisor.cpp
@@ -0,0 +1,20 @@
+// sk
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+          long long t ;
+          cin>>t;
+          while(t--){
+                    long long n;
+                    cin>>n;
+                   while(n%2==0){
+                             n/=2;
+                             
+                   }
+                   if(n==1) cout<<"no\n";
+                   else cout<<"yes\n";
+                   
+          }
+      return 0;
+          
+}
