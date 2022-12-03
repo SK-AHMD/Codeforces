7c4bdafdbabb3f127e5452519f406e202f5e5384
Sheikh Ahmadullah <115889131+SK-AHMD@users.noreply.github.com>
2022-12-03 23:57:46 +0530

    Create Q15 [1606A] AB Balance.cpp

---
+++ C2Ladder/@900/Q15 [1606A] AB Balance.cpp
@@ -0,0 +1,16 @@
+// sk
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        s.back()=s[0];
+        cout<<s<<"\n";
+        
+    }
+    return 0;
+    
+}
