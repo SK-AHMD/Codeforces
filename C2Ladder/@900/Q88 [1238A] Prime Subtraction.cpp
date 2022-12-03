7f3296e0f09052475b5607589c9b6a19297eb39e
Sheikh Ahmadullah <115889131+SK-AHMD@users.noreply.github.com>
2022-12-04 00:51:33 +0530

    Create Q88 [1238A] Prime Subtraction.cpp

---
+++ C2Ladder/@900/Q88 [1238A] Prime Subtraction.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+          int t;
+          cin>>t;while(t--){
+                    long long x,y;
+                    cin>>x>>y;
+                    if(abs(x-y)==1) cout<<"no\n";
+                    else cout<<"yes\n";
+                    
+          }
+          return 0;
+          
+}
