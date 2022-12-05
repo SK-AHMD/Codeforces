11328103d027446ad17ac5fd46120ca9096d74b2
Sheikh Ahmadullah <115889131+SK-AHMD@users.noreply.github.com>
2022-12-05 21:00:32 +0530

    Create Q97 [4A] Watermelon.cpp

---
+++ C2Ladder/@800/Q97 [4A] Watermelon.cpp
@@ -0,0 +1,12 @@
+// sk 
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+          int w;
+          cin>>w;
+          int a = w-2;
+          if(a%2==0 && a!=0) cout<<"yes\n";
+          else cout<<"no\n";
+          return 0;
+          
+}
