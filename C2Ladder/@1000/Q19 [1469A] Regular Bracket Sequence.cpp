3f9b100152cc7ef2075b9364fb66fcd9eabdf261
Sheikh Ahmadullah <115889131+SK-AHMD@users.noreply.github.com>
2022-12-07 11:16:34 +0530

    Create Q19 [1469A] Regular Bracket Sequence.cpp

---
+++ C2Ladder/@1000/Q19 [1469A] Regular Bracket Sequence.cpp
@@ -0,0 +1,18 @@
+// sk 
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+          int t;
+          cin>>t;
+          while(t--){
+                    string s;
+                    cin>>s;
+                    int n = s.size();
+                    if(n%2!=0 || s[0]==')' || s[n-1]=='(') cout<<"NO\n";
+                    else cout<<"YES\n";
+                    
+                    
+          }
+          return 0;
+          
+}
