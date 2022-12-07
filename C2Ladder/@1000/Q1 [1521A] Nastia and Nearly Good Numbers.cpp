91077adf30fcd258a49913e9650be0fca5bee736
Sheikh Ahmadullah <115889131+SK-AHMD@users.noreply.github.com>
2022-12-07 11:10:59 +0530

    Create Q1 [1521A] Nastia and Nearly Good Numbers.cpp

---
+++ C2Ladder/@1000/Q1 [1521A] Nastia and Nearly Good Numbers.cpp
@@ -0,0 +1,16 @@
+// sk    
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+ int t;
+ cin>>t;
+ while(t--){
+    unsigned long long  a,b;
+     cin>>a>>b;
+if(b==1) cout<<"NO\n";
+else  cout<<"YES\n"<<a<<" "<<a*b<<" "<<a*(b+1)<<"\n";
+  }
+ return 0;
+  
+}
