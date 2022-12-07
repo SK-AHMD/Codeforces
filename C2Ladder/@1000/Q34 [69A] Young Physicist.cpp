c5f6043574715bf04c33fc66e51412ba5c574173
Sheikh Ahmadullah <115889131+SK-AHMD@users.noreply.github.com>
2022-12-07 11:18:08 +0530

    Create Q34 [69A] Young Physicist.cpp

---
+++ C2Ladder/@1000/Q34 [69A] Young Physicist.cpp
@@ -0,0 +1,29 @@
+// sheikh
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    
+    int a=0,b=0,c=0;
+
+    while(n--){
+        int x,y,z;
+        cin>>x>>y>>z;
+         a+=x;
+        b+=y;
+        c+=z;
+
+    }
+    if(a==0 && b==0 && c==0){
+        cout<<"YES\n";
+
+    }
+    else {
+        cout<<"NO\n";
+
+    }
+    return 0;
+    
+    
+}
